6cef75ebf0767f73a5268a50f79099a5e074d836
Carlos <carlospe097@outlook.com>
2017-11-25 16:08:59 -0600

    Terminada la funcionalidad del LevelManager

--- Nicaragua Storybook/Assets/Scripts/LevelManager.cs
+++ Nicaragua Storybook/Assets/Scripts/LevelManager.cs
@@ -1,16 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class LevelManager : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 }
