857573e15f0e7b4d3ebabb07d9a575f1ad190084
OldManCharles <carlospe097@outlook.com>
2017-11-26 04:02:13 -0600

    Menu finalizado y se empieza a trabajar minijuego de cartas

---
+++ Nicaragua Storybook/Assets/Scripts/GameCard.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GameCard : MonoBehaviour
+{
+    public enum CardTypes
+    {
+        Nicaragua,
+        Guardabarranco,
+        Madrono,
+        Sacuanjoche
+    }
+
+    public CardTypes Type;
+
+    private Animator _animator;
+    private GameController _controller;
+    private bool _flipped;
+
+
+    // Use this for initialization
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _flipped = false;
+        _controller = FindObjectOfType<GameController>();
+    }
+
+    private void OnMouseDown()
+    {
+        if (!_flipped)
+        {
+            _controller.GameCardSelected(this);
+
+            _animator.Play("Card flip");
+            _flipped = true;
+        }
+    }
+
+    public void OnCardFlipped()
+    {
+        _animator.Play("Card Flipped");
+    }
+
+    public void RevertToOriginalState()
+    {
+        _animator.Play("Card Idle");
+        _flipped = false;
+    }
+}
---
+++ Nicaragua Storybook/Assets/Scripts/GameController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Boo.Lang;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject CardPrefab;
+
+    private readonly List<GameCard> _selectedGameCards = new List<GameCard>();
+
+    private readonly Vector3[] _cardPositions = 
+    {
+        new Vector3(-5, 3),
+        new Vector3(-2, 3),
+        new Vector3(2, 3),
+        new Vector3(5, 3),
+        new Vector3(-5, -3),
+        new Vector3(-2, -3),
+        new Vector3(2, -3),
+        new Vector3(5, -3)
+    };
+
+    private readonly GameCard.CardTypes[] _availableCardTypes = (GameCard.CardTypes[]) Enum.GetValues(typeof(GameCard.CardTypes));
+
+    private void Start()
+    {
+        GameSetUp();
+    }
+
+    private void GameSetUp()
+    {
+        for (var i = 0; i < _cardPositions.Length; i++)
+        {
+            var pos = _cardPositions[i];
+            var card = Instantiate(CardPrefab, pos, Quaternion.identity);
+            switch (i)
+            {
+                case 0:
+                    card.GetComponent<GameCard>().Type = GameCard.CardTypes.Guardabarranco;
+                    break;
+                case 1:
+                    card.GetComponent<GameCard>().Type = GameCard.CardTypes.Guardabarranco;
+                    break;
+                case 2:
+                    card.GetComponent<GameCard>().Type = GameCard.CardTypes.Madrono;
+                    break;
+                case 3:
+                    card.GetComponent<GameCard>().Type = GameCard.CardTypes.Madrono;
+                    break;
+                case 4:
+                    card.GetComponent<GameCard>().Type = GameCard.CardTypes.Nicaragua;
+                    break;
+                case 5:
+                    card.GetComponent<GameCard>().Type = GameCard.CardTypes.Nicaragua;
+                    break;
+                case 6:
+                    card.GetComponent<GameCard>().Type = GameCard.CardTypes.Sacuanjoche;
+                    break;
+                case 7:
+                    card.GetComponent<GameCard>().Type = GameCard.CardTypes.Sacuanjoche;
+                    break;
+            }
+        }
+    }
+
+    private IEnumerator CompareCards(GameCard a, GameCard b)
+    {
+        yield return new WaitForSeconds(1.0f);
+
+        if (a.Type == b.Type)
+        {
+            Destroy(a.gameObject);
+            Destroy(b.gameObject);
+        }
+        else
+        {
+            a.RevertToOriginalState();
+            b.RevertToOriginalState();
+        }
+    }
+
+    public void GameCardSelected(GameCard card)
+    {
+        _selectedGameCards.Add(card);
+
+        if (_selectedGameCards.Count == 2)
+        {
+            StartCoroutine(CompareCards(_selectedGameCards[0], _selectedGameCards[1]));
+            _selectedGameCards.Clear(); //So we don't keep old information
+        }
+    }
+}
