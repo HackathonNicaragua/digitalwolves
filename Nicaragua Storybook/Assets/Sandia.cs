4b989d543c6af6b7debff3653ab39c558bcb2c9f
OldManCharles <carlospe097@outlook.com>
2017-11-26 12:26:25 -0600

    Subida final de Nicaragua Storybook

---
+++ Nicaragua Storybook/Assets/Sandia.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sandia : MonoBehaviour 
+{
+	public int Hp;
+	public AudioClip Clip;
+
+	public LevelManager levelManager;
+
+	private void OnMouseDown()
+	{
+		GetComponent<AudioSource> ().PlayOneShot (Clip);
+		Hp--;
+		GetComponent<Animator> ().Play ("Sandia Comida");
+
+		if (Hp < 1) {
+			levelManager.LoadLevel ("QuizTioCoyote");
+		}
+	}
+}
